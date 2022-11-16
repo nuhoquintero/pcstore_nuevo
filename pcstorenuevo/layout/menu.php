5dd1943ed12f0105526866b63fce7411c41342e3
pakitoflow <danix1405xd@gmail.com>
2022-11-16 10:02:10 -0300

    jhds

---
+++ pcstorenuevo/layout/menu.php
@@ -0,0 +1,13 @@
+<nav>
+        <ul>
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="teclados.php">Teclados</a></li>
+            <li class="carrito">
+                <a href="#" class='btn-carrito'>Carrito</a>
+                <div id="carrito-container">
+                    <div id="tabla">
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </nav>
